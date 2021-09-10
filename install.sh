896dcc0187e573bbe40f1390b7d21aedee0ede20
tktoan2 <89565491+tktoan2@users.noreply.github.com>
2021-09-10 11:36:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+#Delete old system
+docker stop webapp
+docker rm webapp
+echo "Webapp container has been deleted"
+docker stop dbserver
+docker rm dbserver
+echo "Dbserver container has been deleted"
+docker rmi imwebapp
+docker rmi imdbserver
+docker network rm Mynet1
+echo "Network has been deleted"
+#Deploy new system
 docker build --target webapp -t imwebapp .
 docker build --target dbserver -t imdbserver .
 docker run -it -d -p 9001:9000 --name webapp imwebapp
@@ -5,4 +17,7 @@ docker run -it -d -p 1433:1433 --name dbserver imdbserver
 docker network create Mynet1
 docker network connect Mynet1 webapp
 docker network connect Mynet1 dbserver
-echo "Deployment succesfull"
+sleep 15s
+ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+echo "Deployment succesfull, please access http://$ip:9001"
+
