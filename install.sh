494d7d8939929117d413795129cb02741772c0bf
tktoan2 <89565491+tktoan2@users.noreply.github.com>
2021-11-23 14:50:35 +0700

    Add files via upload
    Add docker compose file and edit install.sh file

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ echo "Network has been deleted"
 #Deploy new system
 docker build --target webapp -t imwebapp .
 docker build --target dbserver -t imdbserver .
-docker run -it -d -p 9001:9000 --name webapp imwebapp
-docker run -it -d -p 1433:1433 --name dbserver imdbserver
+docker run -it -d -p 9001:9000 --name webdienthoai_webserver_1 imwebapp
+docker run -it -d -p 1433:1433 --name webdienthoai_sqlserver_1 imdbserver
 docker network create Mynet1
-docker network connect Mynet1 webapp
-docker network connect Mynet1 dbserver
+docker network connect Mynet1 webdienthoai_webserver_1
+docker network connect Mynet1 webdienthoai_sqlserver_1
 sleep 15s
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 echo "Deployment successful, please wait about 30s to initialize the database server then access http://$ip:9001"
